2321b66e30bbcbb1329ff7a0cfa3b07ba674d0ba
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-05-14 21:54:33 +0200

    aggiunti parametri alle istanze.

--- src/main/java/org/lessons/java/inheritance/shop/Shop.java
+++ src/main/java/org/lessons/java/inheritance/shop/Shop.java
@@ -4,15 +4,15 @@ public class Shop {
 
     public static void main(String[] args) {
 	
-	Smartphone smartphone1 = new Smartphone(null, null, 0, 0, null, 0);
+	Smartphone smartphone1 = new Smartphone("Galaxy S21", "Samsung", 800, 22, "123456789ABCDEF", 256);
 	System.out.println(smartphone1);
 	System.out.println(" ");
 	
-	Televisori televisori1 = new Televisori(null, null, 0, 0, null, null);
+	Televisori televisori1 = new Televisori("OLED55B9", "LG", 1200, 22, "55 polici", "Smart tv");
 	System.out.println(televisori1);
 	System.out.println(" ");
 	
-	Cuffie cuffie1 = new Cuffie(null, null, 0, 0, null, null);
+	Cuffie cuffie1 = new Cuffie("WH-1000XM4", "Sony", 350, 22, "Blu", "Wireless");
 	System.out.println(cuffie1);
 	System.out.println(" ");
 	
